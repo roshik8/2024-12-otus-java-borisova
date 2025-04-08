ac04c7e7dbcfaa620bef641b7888955a2c7cabf8
Anna Borisova <roshik8@gmail.com>
2025-04-08 15:19:05 +0500

    hw08-jdbc - init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,8 @@ include("hw04-gc")
 include("hw05-byteCodes")
 include("hw06-oop")
 include("hw07-pattern")
-
 include("hw08-io")
+include("hw09-jdbc")
 
 pluginManagement {
     val jgitver: String by settings
