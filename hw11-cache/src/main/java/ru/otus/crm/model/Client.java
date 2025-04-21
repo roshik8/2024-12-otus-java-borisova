25d5be02a69cc1b8b096ff860d08f880e0706a62
Anna Borisova <roshik8@gmail.com>
2025-04-21 14:40:13 +0500

    hw11-cache - init

---
+++ hw11-cache/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,77 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @SequenceGenerator(name = "client_gen", sequenceName = "client_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(mappedBy = "client", fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    private List<Phone> phones;
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+        this.phones = new ArrayList<>();
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.phones = new ArrayList<>();
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = new ArrayList<>(phones);
+        this.phones.forEach(p -> p.setClient(this));
+    }
+
+    @Override
+    @SuppressWarnings({"java:S2975", "java:S1182"})
+    public Client clone() {
+        Address clonedAddress = address == null ? null : new Address(address.getId(), address.getStreet());
+
+        List<Phone> clonedPhones = phones == null ? null : new ArrayList<>();
+        if (phones != null) {
+            for (Phone phone : phones) {
+                Phone clonedPhone = new Phone(phone.getId(), phone.getNumber());
+                clonedPhones.add(clonedPhone);
+            }
+        }
+
+        Client clonedClient = new Client(this.id, this.name, clonedAddress, clonedPhones);
+        clonedPhones.forEach(p -> p.setClient(clonedClient));
+
+        return clonedClient;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
+}
