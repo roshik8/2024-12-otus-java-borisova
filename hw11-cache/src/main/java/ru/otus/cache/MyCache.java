79d1ba4eb54ccc8aeed70810d8a6adc205d7a8d4
Anna Borisova <roshik8@gmail.com>
2025-10-01 09:37:21 +0500

    hw11-cache - work

--- hw11-cache/src/main/java/ru/otus/cache/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cache/MyCache.java
@@ -1,30 +1,62 @@
 package ru.otus.cache;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MyCache<K, V> implements HwCache<K, V> {
-    // Надо реализовать эти методы
+    private static final Logger log = LoggerFactory.getLogger(MyCache.class);
+    private final Map<K, V> cache = new WeakHashMap<>();
+    private final List<HwListener<K, V>> listeners = new ArrayList<>();
 
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        cache.put(key, value);
+        notifyListeners(key, value, "put");
     }
 
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException();
+        notifyListeners(key, cache.get(key), "remove");
+        cache.remove(key);
     }
 
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        V value = cache.get(key);
+        notifyListeners(key, value, "get");
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.add((listener));
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.remove(listener);
+    }
+
+    public int size() {
+        return cache.size();
+    }
+
+    private void notifyListeners(K key, V value, String action) {
+        listeners.forEach(listener -> {
+            try {
+                listener.notify(key, value, action);
+            } catch (Exception e) {
+                log.error(
+                        "Error calling listener notify with params " + "key: {} " + "value: {} " + "action: {}",
+                        key,
+                        value,
+                        action,
+                        e);
+            }
+        });
     }
 }
--- hw11-cache/src/main/java/ru/otus/demo/DbServiceDemo.java
+++ hw11-cache/src/main/java/ru/otus/demo/DbServiceDemo.java
@@ -3,6 +3,10 @@ package ru.otus.demo;
 import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.shaded.org.apache.commons.lang3.time.StopWatch;
+import ru.otus.cache.HwCache;
+import ru.otus.cache.HwListener;
+import ru.otus.cache.MyCache;
 import ru.otus.core.repository.DataTemplateHibernate;
 import ru.otus.core.repository.HibernateUtils;
 import ru.otus.core.sessionmanager.TransactionManagerHibernate;
@@ -10,7 +14,13 @@ import ru.otus.crm.dbmigrations.MigrationsExecutorFlyway;
 import ru.otus.crm.model.Address;
 import ru.otus.crm.model.Client;
 import ru.otus.crm.model.Phone;
+import ru.otus.crm.service.DBServiceClient;
+import ru.otus.crm.service.DbServiceClientCache;
 import ru.otus.crm.service.DbServiceClientImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class DbServiceDemo {
 
@@ -19,6 +29,18 @@ public class DbServiceDemo {
     public static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
 
     public static void main(String[] args) {
+
+        var dbCacheServiceClient = createDBServiceClient(true);
+        //намеренно вызываем метод получить всех, чтобы значения сложились в кэш
+        var allClients = dbCacheServiceClient.findAll();
+        var dbServiceClient = createDBServiceClient(false);
+
+        var clientCount = allClients.size();
+        System.out.println("Время выполнения с кэшем: " + culcExecuteTime(dbCacheServiceClient, clientCount));
+        System.out.println("Время выполнения без кэша: " + culcExecuteTime(dbServiceClient, clientCount));
+        //Время выполнения с кэшем 270, без кеша 18887
+    }
+    private static DBServiceClient createDBServiceClient(boolean needCache) {
         var configuration = new Configuration().configure(HIBERNATE_CFG_FILE);
 
         var dbUrl = configuration.getProperty("hibernate.connection.url");
@@ -26,29 +48,24 @@ public class DbServiceDemo {
         var dbPassword = configuration.getProperty("hibernate.connection.password");
 
         new MigrationsExecutorFlyway(dbUrl, dbUserName, dbPassword).executeMigrations();
-
         var sessionFactory = HibernateUtils.buildSessionFactory(configuration, Client.class, Address.class, Phone.class);
-
         var transactionManager = new TransactionManagerHibernate(sessionFactory);
-        ///
         var clientTemplate = new DataTemplateHibernate<>(Client.class);
-        ///
-        var dbServiceClient = new DbServiceClientImpl(transactionManager, clientTemplate);
-        dbServiceClient.saveClient(new Client("dbServiceFirst"));
+        if (needCache) {
+            HwCache<Long, Client> cache = new MyCache<>();
+            HwListener<Long, Client> listener = (key, value, action) -> log.info("key: {}, value: {}, action: {}", key, value, action);
+            cache.addListener(listener);
+            return new DbServiceClientCache(transactionManager, clientTemplate, cache);
+        } else {
+            return new DbServiceClientImpl(transactionManager, clientTemplate);
+        }
+    }
 
-        var clientSecond = dbServiceClient.saveClient(new Client("dbServiceSecond"));
-        var clientSecondSelected = dbServiceClient
-                .getClient(clientSecond.getId())
-                .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecond.getId()));
-        log.info("clientSecondSelected:{}", clientSecondSelected);
-        ///
-        dbServiceClient.saveClient(new Client(clientSecondSelected.getId(), "dbServiceSecondUpdated"));
-        var clientUpdated = dbServiceClient
-                .getClient(clientSecondSelected.getId())
-                .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecondSelected.getId()));
-        log.info("clientUpdated:{}", clientUpdated);
-
-        log.info("All clients");
-        dbServiceClient.findAll().forEach(client -> log.info("client:{}", client));
+    private static long culcExecuteTime(DBServiceClient serviceClient, int clientCount) {
+        long startTime = System.currentTimeMillis();
+        for( int i = 0; i < clientCount; i++ ) {
+            serviceClient.getClient(clientCount);
+        }
+        return System.currentTimeMillis() - startTime;
     }
 }
