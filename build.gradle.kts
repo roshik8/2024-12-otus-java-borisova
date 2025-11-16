66825a84f9c9041099c34c456ce02ed3c6dee252
Anna Borisova <roshik8@gmail.com>
2025-11-16 20:41:09 +0500

    hw17 - grpc - init

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ allprojects {
     val testcontainersBom: String by project
     val protobufBom: String by project
     val guava: String by project
+    val errorProneAnnotations: String by project
+    val grpc: String by project
 
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
@@ -44,6 +46,10 @@ allprojects {
                 mavenBom("com.google.protobuf:protobuf-bom:$protobufBom")
             }
             dependency("com.google.guava:guava:$guava")
+            dependency("com.google.errorprone:error_prone_annotations:$errorProneAnnotations")
+            dependency("io.grpc:grpc-netty:$grpc")
+            dependency("io.grpc:grpc-protobuf:$grpc")
+            dependency("io.grpc:grpc-stub:$grpc")
         }
     }
 
---
+++ hw17-grpc/src/main/java/ru/otus/numbers/GRPCClient.java
@@ -0,0 +1,69 @@
+package ru.otus.numbers;
+
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
+@SuppressWarnings({"squid:S106"})
+public class GRPCClient {
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 8190;
+    private static final int FIRST_VALUE = 0;
+    private static final int LAST_VALUE = 30;
+    private static final int COUNT = 50;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("работа с числами началась...");
+
+        var channel =
+                ManagedChannelBuilder.forAddress(HOST, PORT).usePlaintext().build();
+
+        var asyncStub = NumbersServiceGrpc.newStub(channel);
+
+        var latch = new CountDownLatch(1);
+        AtomicLong lastServerValue = new AtomicLong(0);
+        AtomicBoolean newValueAvailable = new AtomicBoolean(false);
+
+        asyncStub.streamNumber(
+                NumbersRequest.newBuilder().setFirstValue(FIRST_VALUE).setLastValue(LAST_VALUE).build(),
+                new StreamObserver<NumbersResponse>() {
+                    @Override
+                    public void onNext(NumbersResponse value) {
+                        long v = value.getValue();
+                        lastServerValue.set(v);
+                        newValueAvailable.set(true);
+                        System.out.println("новое значение: " + v);                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        t.printStackTrace();
+                        latch.countDown();
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("запрос отработал");
+                        latch.countDown();
+                    }
+                });
+
+        long currentValue = 0;
+        for (int i = 0; i < COUNT; i++) {
+            if (newValueAvailable.getAndSet(false)) {
+                currentValue = currentValue + lastServerValue.get() + 1;
+            } else {
+                currentValue = currentValue + 1;
+            }
+            System.out.println("текущее значение: " + currentValue);
+            Thread.sleep(1000);
+        }
+        latch.await();
+
+        System.out.println("работа с числами закончилась...");
+        channel.shutdown();
+    }
+}
